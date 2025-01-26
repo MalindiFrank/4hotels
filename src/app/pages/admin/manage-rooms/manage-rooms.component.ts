82bb92d3d15a58715352e757226616918984d124
thembalakhe <m.themba1738@gmail.com>
2025-01-26 10:50:48 +0200

    setup folder structure layout

---
+++ src/app/pages/admin/manage-rooms/manage-rooms.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-rooms',
+  imports: [],
+  templateUrl: './manage-rooms.component.html',
+  styleUrl: './manage-rooms.component.css'
+})
+export class ManageRoomsComponent {
+
+}
