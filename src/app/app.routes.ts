ffae318b9d664c816083886f2264f59cb6cb90fd
thembalakhe <m.themba1738@gmail.com>
2025-01-26 11:37:53 +0200

    setup routing to pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,67 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { authGuard } from './auth/auth.guard';
+import { NgModule } from '@angular/core';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'rooms',
+    loadComponent: () =>
+      import('./pages/rooms/rooms.component').then((m) => m.RoomsComponent),
+  },
+  {
+    path: 'rooms/:id',
+    loadComponent: () =>
+      import('./pages/rooms/rooms.component').then((m) => m.RoomsComponent),
+  },
+  {
+    path: 'booking',
+    loadComponent: () =>
+      import('./pages/booking/booking.component').then(
+        (m) => m.BookingComponent
+      ),
+  },
+  {
+    path: 'feedback',
+    loadComponent: () =>
+      import('./pages/feedback/feedback.component').then(
+        (m) => m.FeedbackComponent
+      ),
+  },
+  {
+    path: 'admin',
+    loadComponent: () =>
+      import('./pages/admin/homepage/homepage.component').then(
+        (m) => m.HomepageComponent
+      ),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'admin/manage-rooms',
+    loadComponent: () =>
+      import('./pages/admin/manage-bookings/manage-bookings.component').then(
+        (m) => m.ManageBookingsComponent
+      ),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'admin/manage-bookings',
+    loadComponent: () =>
+      import('./pages/admin/manage-rooms/manage-rooms.component').then(
+        (m) => m.ManageRoomsComponent
+      ),
+    canActivate: [authGuard],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
