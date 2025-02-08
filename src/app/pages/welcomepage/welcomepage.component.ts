2d0c1c54ff61facbe4b99eb036d037026c2e460a
thembalakhe <m.themba1738@gmail.com>
2025-02-08 16:38:32 +0200

    populated room deatailed view

--- src/app/pages/welcomepage/welcomepage.component.ts
+++ src/app/pages/welcomepage/welcomepage.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../../components/header/header.component";
+import { RouterModule } from '@angular/router';
+// import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-welcomepage',
-  imports: [HeaderComponent],
+  imports: [CommonModule, RouterModule],
   templateUrl: './welcomepage.component.html',
   styleUrl: './welcomepage.component.css'
 })
 export class WelcomepageComponent {
+
+  // constructor(
+  //   router: RouterLink
+  // ){}
 
 }
