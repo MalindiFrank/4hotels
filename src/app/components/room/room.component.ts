111c799f733c3a6f8bb255ee1ef5e7de0cae00f5
thembalakhe <m.themba1738@gmail.com>
2025-02-09 11:14:29 +0200

    adding mock html structure for few temps, awaiting db

--- src/app/components/room/room.component.ts
+++ src/app/components/room/room.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-rooms',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './room.component.html',
   styleUrl: './room.component.css'
 })
--- src/app/pages/welcomepage/welcomepage.component.ts
+++ src/app/pages/welcomepage/welcomepage.component.ts
@@ -1,18 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-// import { RouterLink, RouterModule } from '@angular/router';
+import { DisplayComponent } from "../../components/feedback/display/display.component";
+import { FormComponent } from "../../components/feedback/form/form.component";
+
 
 @Component({
   selector: 'app-welcomepage',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, DisplayComponent, FormComponent],
   templateUrl: './welcomepage.component.html',
   styleUrl: './welcomepage.component.css'
 })
 export class WelcomepageComponent {
 
-  // constructor(
-  //   router: RouterLink
-  // ){}
+  constructor(){}
 
 }
