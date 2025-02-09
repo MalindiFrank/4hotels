a466613cf9ca72c624cf79eb3060370d8853264e
thembalakhe <m.themba1738@gmail.com>
2025-02-09 18:50:37 +0200

    removed routerlink not used yet

--- src/app/components/feedback/display/display.component.ts
+++ src/app/components/feedback/display/display.component.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-display',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './display.component.html',
   styleUrl: './display.component.css'
 })
